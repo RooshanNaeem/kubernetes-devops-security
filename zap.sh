bd881878405e00f578abf3fed786f071da59d318
RooshanNaeem <rooshannaeem@hotmail.com>
2022-09-20 18:46:28 +0500

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
-
+#PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+PORT=8080
 # first run this
 chmod 777 $(pwd)
 echo $(id -u):$(id -g)
