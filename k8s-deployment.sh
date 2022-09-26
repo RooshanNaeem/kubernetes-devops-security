cc51daa8228aa8276797a5a40252b3bb8a4f79e0
RooshanNaeem <rooshannaeem@hotmail.com>
2022-09-26 13:01:27 +0500

    Update k8s-deployment.sh

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -3,14 +3,14 @@
 sed -i "s#replace#${imagaeName}#g" k8s_deployment_service.yaml
 kubectl -n default get deployment/${deploymentName}
 
-#if [[ $? -ne 0 ]]; 
-#then
-#	echo "deployment ${deploymentName} doesn't exist"
-#	kubectl -n default apply -f k8s_deployment_service.yaml
-#else
-#	echo "deployment ${deploymentName} exists" 
-#	echo "image name - ${imageName}"
-#	kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
-#
-#fi
-kubectl -n default apply -f k8s_deployment_service.yaml
+if [[ $? -ne 0 ]]; 
+then
+	echo "deployment ${deploymentName} doesn't exist"
+	kubectl -n default apply -f k8s_deployment_service.yaml
+else
+	echo "deployment ${deploymentName} exists" 
+	echo "image name - ${imageName}"
+	kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
+
+fi
+#kubectl -n default apply -f k8s_deployment_service.yaml
