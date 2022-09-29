ee2119a852f5db74bebf261e8f454b9bcd17572c
RooshanNaeem <rooshannaeem@hotmail.com>
2022-09-29 13:04:27 +0500

    Typo fix

--- cis-kubelet.sh
+++ cis-kubelet.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-total_fail= $(kube-bench run --targets node --version 1.15 --check 4.2.1,4.2.2 --json | jq .[].tatl_fail)
+total_fail= $(kube-bench run --targets node --version 1.15 --check 4.2.1,4.2.2 --json | jq .[].total_fail)
 
 if [[ "$total_fail" -ne 0  ]];
 	then
 		echo "CIS Benchmark failed for testing Kubelet for checks 4.2.1,4.2.2"
 		exit 1;
 	else
-		echo "CIS BEnchmark passed for Kubelet - 4.2.1,4.2.2"
+		echo "CIS Benchmark passed for Kubelet - 4.2.1,4.2.2"
 
 fi;	
--- cis-master.sh
+++ cis-master.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-total_fail= $(kube-bench run --targets master --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .[].tatl_fail)
+total_fail= $(kube-bench run --targets master --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .[].total_fail)
 
 if [[ "$total_fail" -ne 0  ]];
 	then
 		echo "CIS Benchmark failed for testing Master for checks 1.2.7, 1.2.8, 1.2.9"
 		exit 1;
 	else
-		echo "CIS BEnchmark passed for master - 1.2.7, 1.2.8, 1.2.9"
+		echo "CIS Benchmark passed for master - 1.2.7, 1.2.8, 1.2.9"
 
 fi;	
