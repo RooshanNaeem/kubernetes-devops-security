6c5c7d54c59b075b1dbf1d4df0aa9f9ee379753e
RooshanNaeem <rooshannaeem@hotmail.com>
2022-09-29 17:28:01 +0500

    Update cis-etcd.sh

--- cis-etcd.sh
+++ cis-etcd.sh
@@ -2,13 +2,12 @@
 
 
 result= $(kube-bench run --targets etcd --version 1.15 --check 2.2 --json | jq )
-echo $result
 
-#if [[ "$result" -ne 0  ]];
-#	then
-#		echo "CIS Benchmark failed for testing Etcd for check 2.2"
-#		exit 1;
-#	else
-#		echo "CIS Benchmark passed for Etcd - 2.2"
-#
-#fi;	
+if [[ "$result" -ne 0  ]];
+	then
+		echo "CIS Benchmark failed for testing Etcd for check 2.2"
+		exit 1;
+	else
+		echo "CIS Benchmark passed for Etcd - 2.2"
+
+fi;	
