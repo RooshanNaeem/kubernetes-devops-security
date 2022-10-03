f28394c4a072bee1d183361361665fe37f28a96e
RooshanNaeem <rooshannaeem@hotmail.com>
2022-10-03 17:55:13 +0500

    Update k8s-deployment-rollout-status.sh

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -3,12 +3,12 @@
 sleep 60s
 
 if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; then
-	echo "Deployment ${deploymentName} rollout has failed"
+	echo 'Deployment ${deploymentName} rollout has failed'
 	kubectl -n default rollout undo deploy ${deploymentName}
 	exit 1;
 		
 else
-	echo "Deployemnt ${deploymentName} rollout is Success"
+	echo 'Deployemnt ${deploymentName} rollout is Success'
 	
 
 fi
