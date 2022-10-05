4dda4215d58e9ca33ecf74dbc3d493b9f34dafb6
RooshanNaeem <rooshannaeem@hotmail.com>
2022-10-05 14:42:58 +0500

    Update k8s-Prod-deployment-rollout-status.sh

--- k8s-Prod-deployment-rollout-status.sh
+++ k8s-Prod-deployment-rollout-status.sh
@@ -2,7 +2,7 @@
 
 sleep 60s
 
-if [[ $(kubectl -n prod rollout status deploy ${proddeploymentName}--timeout 5s) != *"successfully rolled out"* ]];
+if [[ $(kubectl -n prod rollout status deploy ${proddeploymentName} --timeout 5s) != *"successfully rolled out"* ]]
 then
 	echo "Deployemnt ${proddeploymentName} rollout has failed"
 	kubectl -n prod rollout undo deploy ${proddeploymentName}
